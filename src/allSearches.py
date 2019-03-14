ea275e9bd5aca803a47b8f340ebfcdf42b15d0ee
Mario Fentler <mfentler@student.tgm.ac.at>
2019-03-14 14:30:45 +0100

    Combined ek added

--- src/allSearches.py
+++ src/allSearches.py
@@ -1,19 +1,20 @@
 from horizontalSearch import connect as hconnect
+from horizontalSearch import ek as ekH
 from verticalSearch import connect as vconnect
+from verticalSearch import ek as ekV
 from combinedSearch import connect as cconnect
-
-from horizontalSearch import ek as hek
-from verticalSearch import ek as vek
-from combinedSearch import ek as cek
+from combinedSearch import ek as ekC
 
 if __name__=='__main__':
     print('Horizontal Schema ...')
     hconnect()
+    print('Horizontal Schema Counts ...')
+    ekH()
     print('Vertical Schema ...')
     vconnect()
+    print('Vertical Schema Counts ...')
+    ekV()
     print('Combined Schema ...')
     cconnect()
-    print('....!! Now comes the EK !!....')
-    hek()
-    vek()
-    cek()
+    print('Combined Schema Counts ...')
+    ekC()
--- src/combinedSearch.py
+++ src/combinedSearch.py
@@ -34,9 +34,58 @@ def connect():
         if conn is not None:
             conn.close()
             print('Database connection closed.')
-
 def ek():
-    pass
+    conn = None
+    try:
+        # read connection parameters
+        params = config('postgresql:combined')
 
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('Number of columns in combined table:')
+        cur.execute("SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_CATALOG = 'ds2' AND TABLE_SCHEMA = 'combination' AND TABLE_NAME = 'cheap_comedy_simplified';")
+        dvds = cur.fetchall()
+        for x in dvds:
+            print(x)
+        print('Number of columns in the standard table:')
+        cur.execute("SELECT count(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_CATALOG = 'ds2' AND TABLE_NAME = 'products';")
+        dvds = cur.fetchall()
+        for x in dvds:
+            print(x)
+        print('Number of columns in the standard table without the excluded values:')
+        cur.execute("SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_CATALOG = 'ds2' AND TABLE_NAME = 'products' AND COLUMN_NAME NOT IN (SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_CATALOG = 'ds2' AND TABLE_SCHEMA = 'combination' AND TABLE_NAME = 'cheap_comedy_simplified');")
+        dvds = cur.fetchall()
+        for x in dvds:
+            print(x)
+        print('Number of entries in combined table:')
+        cur.execute('SELECT count(*) FROM combination.cheap_comedy_simplified;')
+        dvds = cur.fetchall()
+        for x in dvds:
+            print(x)
+        print('Number of entries in the standard table:')
+        cur.execute('SELECT count(*) FROM products;')
+        dvds = cur.fetchall()
+        for x in dvds:
+            print(x)
+        print('Number of entries in the standard table without the excluded values:')
+        cur.execute('SELECT count(*) FROM products WHERE prod_id NOT IN (SELECT prod_id FROM combination.cheap_comedy_simplified);')
+        dvds = cur.fetchall()
+        for x in dvds:
+            print(x)
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
 if __name__ == '__main__':
     connect()
+    ek()
